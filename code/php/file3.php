f29f0bc1bfd0f6154325ab39200b15ccc46f2b25
JohnPapagian <75178757+JohnPapagian@users.noreply.github.com>
2020-12-08 21:58:08 +0200

    Update file3.php

--- code/php/file3.php
+++ code/php/file3.php
@@ -9,7 +9,7 @@ $conn = dbConnect();
 
 if (!$conn) {
    
-    die('Could not connect: ' . mysql_error());
+    die('Could not connect: ' . mysqli_error());
   }
   
 ini_set('display_errors', 1);
@@ -17,65 +17,71 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-$array1=($_POST['pinakas']);
 
+$array1=$_POST['pinakas'];
 $arrayLength = count($array1);
-echo ($arrayLength);
-echo("\n\n\n");
 
 
 
 
+   if(is_array($array1)){
 
+    $sql = "INSERT INTO data (useremail,startedDateTime,wait,serverIPAddress,method,domainname,status,statusText,contenttype,cachecontrol,pragma,expires,age,lastmodified,host,paroxos,city,geoloc) VALUES ";
+    $i=0;
+    $valuesArr = array();
+    foreach($array1 as $row)
+{
+		if ( is_array($row)){
 
+			
+			$email=mysqli_real_escape_string($conn,$row['email']);
+			
+			$startedDateTime=  mysqli_real_escape_string($conn,$row['startedDateTime']);
+		
+			$serverIPAddress=mysqli_real_escape_string($conn,$row['IpAddress']);
+		
+			$wait=mysqli_real_escape_string($conn,$row['wait']);
+			
+			$Status=mysqli_real_escape_string($conn,$row['Status']);
 
-$i = 1;
-        while ($i < $arrayLength)
-        {   
-		    echo ($i);
-			echo("\n");
-            $email=$array1[$i]['email'];
-            $startedDateTime=  $array1[$i]['startedDateTime'];
-            $wait=$array1[$i]['wait'];
-            $IpAddress= $array1[$i]['IpAddress'];
-            $Status= $array1[$i]['Status'];
-            $StatusText = $array1[$i]['StatusText'];
-            $geo_loc= $array1[$i]['geo_loc'];
-            $method=  $array1[$i]['method'];
-            $url=$array1[$i]['url'];
-            $cacheControl=$array1[$i]['cacheControl'];
-            $ContentType= $array1[$i]['ContentType'];
-            $pragma= $array1[$i]['pragma'];
-            $expires= $array1[$i]['expires'];
-            $lastmod=$array1[$i]['lastmod'];
-            $host= $array1[$i]['host'];
-            $age=  $array1[$i]['age'];
-            $city=  $array1[$i]['city'];
-            $paroxos= $array1[$i]['paroxos'];
-			echo ($startedDateTime);
-			echo("\n");
-            $i++;
-	
-		$sql='INSERT INTO data(useremail,startedDateTime,wait,serverIPAddress,method,domainname,status,statusText,contenttype,cachecontrol,pragma,expires,age,lastmodified,host,paroxos,city,geoloc) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        $stmt = $conn->prepare($sql);
-  
+            $StatusText = mysqli_real_escape_string($conn, $row['StatusText']);
+            
+			$geo_loc= mysqli_real_escape_string($conn, $row['geo_loc']);
+            
+			$method=  mysqli_real_escape_string($conn, $row['method']);
+            
+			$url=mysqli_real_escape_string($conn, $row['url']);
+            
+			$cacheControl=mysqli_real_escape_string($conn, $row['cacheControl']);
+            
+			$ContentType= mysqli_real_escape_string($conn, $row['ContentType']);
+            
+			$pragma= mysqli_real_escape_string($conn, $row['pragma']);
+            
+			$expires= mysqli_real_escape_string($conn, $row['expires']);
+            
+			$lastmod=mysqli_real_escape_string($conn, $row['lastmod']);
+            
+			$host= mysqli_real_escape_string($conn, $row['host']);
+            
+			$age=  mysqli_real_escape_string($conn, $row['age']);
+            
+			$city=  mysqli_real_escape_string($conn, $row['city']);
+            
+			$paroxos= mysqli_real_escape_string($conn, $row['paroxos']);
+        
+		$valuesArr[$i] = "('$email','$startedDateTime','$wait','$serverIPAddress','$method','$url','$Status','$StatusText','$ContentType','$cacheControl','$pragma','$expires','$age','$lastmod','$host', '$paroxos', '$city', '$geo_loc')";
 
+		$i=$i+1;
+	}
+	}
 
-  $stmt->bind_param('ssssssssssssssssss',$email,$startedDateTime,$wait,$serverIPAddress,$method,$url,$Status,$StatusText,$ContentType,$cacheControl,$pragma,$expires,$age,$lastmod,$host, $paroxos, $city, $geo_loc);
-  $stmt->execute();
-  $stmt->close();
-
-
-
-
-  }
-
-
-
-
-   
-  
-
-
+	$sql .= implode(',', $valuesArr);
+    echo("\n\n");
+	echo("query sent");
+    mysqli_query($conn,$sql) or exit(mysqli_error($conn)); 
+}
+		     
+?>
 
  
