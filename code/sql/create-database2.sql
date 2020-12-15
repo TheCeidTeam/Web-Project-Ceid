3f0163785e84fd377db76920eecc2631af88b4e7
JohnPapagian <75178757+JohnPapagian@users.noreply.github.com>
2020-12-15 20:05:46 +0200

    Add files via upload

---
+++ code/sql/create-database2.sql
@@ -0,0 +1,52 @@
+DROP DATABASE IF EXISTS hardb;
+
+CREATE DATABASE hardb;
+
+
+DROP TABLE IF EXISTS `users` ;
+CREATE TABLE IF NOT EXISTS  `users` (
+  `password` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `username` VARCHAR(255) NOT NULL,
+  `type` ENUM('user', 'admin') NOT NULL DEFAULT 'user',
+  `firstname` VARCHAR(45) NOT NULL,
+  `lastname` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`email`)
+ )
+ENGINE = MariaDB
+DEFAULT CHARACTER SET = utf8;
+
+
+DROP TABLE IF EXISTS `data` ;
+CREATE TABLE IF NOT EXISTS `data` (
+  `useremail` VARCHAR(255) NOT NULL,
+  `startedDateTime` VARCHAR(255) NOT NULL,
+  `wait` VARCHAR(255),
+  `serverIPAddress` VARCHAR(255) NOT NULL,
+  `method` VARCHAR(45) NOT NULL,
+  `domainname` VARCHAR(255) NOT NULL,
+  `status` VARCHAR(255) NOT NULL,
+  `statusText` VARCHAR(255) NOT NULL,
+  `contenttype` VARCHAR(255) NOT NULL,
+  `cachecontrol` VARCHAR(255) NOT NULL,
+  `pragma` VARCHAR(255) NOT NULL,
+  `expires` VARCHAR(255) NOT NULL,
+  `age` VARCHAR(255) NOT NULL ,
+  `lastmodified` VARCHAR(255) NOT NULL,
+  `host` VARCHAR(255) NOT NULL,
+  `paroxos` VARCHAR(255),
+  `city` VARCHAR(255),
+  `geoloc` VARCHAR(255),
+  `dataid` VARCHAR(255),
+  `datadate` DATE,
+  `server_loc`  VARCHAR(255),
+  PRIMARY KEY (`startedDateTime`, `useremail`,`dataid`,`wait`),
+   CONSTRAINT `actuser` FOREIGN KEY (`useremail`) REFERENCES `users`(`email`)
+    ON DELETE CASCADE ON UPDATE CASCADE
+  )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+
+INSERT INTO `users` VALUES('1234!Asss','geodimyolo@gmail.com','goedim','user','geo','dim');
